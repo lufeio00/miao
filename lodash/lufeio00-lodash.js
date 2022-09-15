603233f87823f9bebf3197561e7260652c1d02e0
zhouyueji <1121010118@qq.com>
2022-09-15 10:20:41 +0800

    2

--- lodash/lufeio00-lodash.js
+++ lodash/lufeio00-lodash.js
@@ -1,10 +1,8 @@
 var lufeio00 = {
-  chunk:function(array,size){
-    var r=[]
-    var t=[]
-    var j=0
+  chunk:function(array,size=1){
+    var r=[], t=[],j=0
     for(var i=0;i<array.length;i++){
-      if(j==size||i==array.length){
+      if(j==size){
         r.push(t)
         t=[]
         j=0
@@ -12,7 +10,81 @@ var lufeio00 = {
       t.push(array[i])
       j++
     }
+    r.push(t)
     return r
-  }
+  },
+
+  compact:function(array){
+    var r=[]
+    for(var i=0;i<array.length;i++){
+      if(array[i]){
+        r.push(array[i])
+      }
+    }
+    return r
+  },
+
+ /*  difference:function(array,values){}, */
+
+  fill:function(array,value,start=0,end=array.length){
+    for(var i=start;i<end;i++){
+      array[i]=value
+    }
+    return array
+  },
+
+  drop:function(array,n=1){
+    for(var i=0;i<n;i++){
+      array.shift()
+    }
+    return array
+  },
+
+/*   findIndex:function(array){}, */
+
+/*   findLastIndex: function(){}, */
+
+  flatten:function(array){
+    var r=[]
+    for(var i=0;i<array.length;i++){
+      if(Array.isArray(array[i])){
+        for(var j=0;j<array[i].length;j++){
+          r.push(array[i][j])
+        }
+      }else{
+        r.push(array[i])
+      }
+    }
+    return r
+  },
+
+  flattenDeep:function(array){
+    var r=[]
+    y(array)
+    function y(array){
+      for(var i=0;i<array.length;i++){
+        if(Array.isArray(array[i])){
+          y(array[i])
+        }else{
+          r.push(array[i])
+        }
+      }
+    }
+    return r
+  },
+
+  flattenDepth:function(array,depth=1){
+    var r=[]
+
+  },
+
+
+
+
+
+
+
+
+
 
 }
